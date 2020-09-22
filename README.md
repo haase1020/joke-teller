a37e7e6ca0b156a8fa04eb8c6281abaa178f6df3
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-22 09:18:24 -0400

    good for now

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# joke-teller
+# joke-teller or "Make Me Laugh"
+
 JavaScript app for telling jokes
+
+## About this app
+
+- A vanilla JavaScript app using a fetch call to a joke api
+- This app uses Voice RSS Text-to-Speech(TTS) API `http://www.voicerss.org/api/`
+- This app is the fourth project in the JavaScript Web Projects: 20 Projects to Build Your Portfolio by [Zero To Mastery Academy](https://zerotomastery.io/)
+  ![joke-teller](favicon.png)
--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function tellMe(joke) {
   const jokeString = joke.trim().replace(/ /g, '%20');
   // VoiceRSS Speech Parameters
   VoiceRSS.speech({
-    key: 'YOUR_API_KEY_HERE',
+    key: 'ca5385e070d942118d64a1106da0ae26',
     src: jokeString,
     hl: 'en-us',
     r: 0,
@@ -24,7 +24,8 @@ function tellMe(joke) {
 // Get jokes from Joke API
 async function getJokes() {
   let joke = '';
-  const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
+  const apiUrl =
+    'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
